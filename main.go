eb3e5c2b4cb7f486e2156b76ce6b2d9e56629c1d
Harrison Lloyd <harrisonalloyd@gmail.com>
2025-10-10 17:53:48 -0700

    Now returing JSON with my name displayed

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 import "github.com/gofiber/fiber/v2"
 
+func root(c *fiber.Ctx) error {
+	return c.JSON(fiber.Map{
+		"message": "My name is Harrison Lloyd"
+	})
+}
+
 func main() {
 	app := fiber.New()
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("hello world")
-	})
-
+	app.Get("/", root) // instance of a Fiber application
 	app.Listen(":8080")
 }
