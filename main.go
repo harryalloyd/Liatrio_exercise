c90e41afa266f6842faada770b1267edb73cf5de
Harrison Lloyd <harrisonalloyd@gmail.com>
2025-10-10 18:14:09 -0700

    Fixed issue with missing comma. Added the timestamp to the JSON response

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 import "github.com/gofiber/fiber/v2"
+import "time"
 
 func root(c *fiber.Ctx) error {
 	return c.JSON(fiber.Map{
-		"message": "My name is Harrison Lloyd"
+		"message": "My name is Harrison Lloyd",
+		"timestamp": time.Now().Unix(), //using unix to keep it as an int
 	})
 }
 
