74c619ec174b18437ec602ee419e4915c53a7cc8
Harrison Lloyd <harrisonalloyd@gmail.com>
2025-10-15 21:47:42 -0700

    ci test v12, set main back to original self

--- main.go
+++ main.go
@@ -2,20 +2,15 @@ package main
 import "github.com/gofiber/fiber/v2"
 import "time"
 
-type Payload struct {
-	Message   string `json:"message"`
-	Timestamp int64  `json:"timestamp"`
+func root(c *fiber.Ctx) error {
+	return c.JSON(fiber.Map{
+		"message": "My name is Harrison Lloyd",
+		"timestamp": time.Now().UnixMilli(), //using unix to keep it as an int
+	})
 }
 
 func main() {
 	app := fiber.New()
-	app.Get("/", func(c *fiber.Ctx) error {
-		p := Payload{
-			Message:   "My name is Harrison Lloyd",
-			Timestamp: time.Now().UnixMilli(), 
-		}
-		return c.JSON(p) 
-	})
-
+	app.Get("/", root) // instance of a Fiber application
 	app.Listen(":8080")
 }
