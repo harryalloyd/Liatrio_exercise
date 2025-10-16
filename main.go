cbe5e4a76c5c9da73844e6f61c0453a0f644590b
Harrison Lloyd <harrisonalloyd@gmail.com>
2025-10-15 21:02:17 -0700

    ci test v9

--- main.go
+++ main.go
@@ -1,25 +1,22 @@
 package main
 import "github.com/gofiber/fiber/v2"
 import "time"
-import "encoding/json"
 
 type Payload struct {
-	Message string `json:"message"`
+	Message   string `json:"message"`
 	Timestamp int64  `json:"timestamp"`
-}
-
-func root(c *fiber.Ctx) error {
-	resp := Payload{
-		Message: "My name is Harrison Lloyd",
-		Timestamp: time.Now().UnixMilli(), 
-	}
-	b, _ := json.Marshal(resp) 
-	c.Set(fiber.HeaderContentType, fiber.MIMEApplicationJSON)
-	return c.Send(b)
 }
 
 func main() {
 	app := fiber.New()
-	app.Get("/", root) 
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		p := Payload{
+			Message:   "My name is Harrison Lloyd",
+			Timestamp: time.Now().Unix(), 
+		}
+		return c.JSON(p) 
+	})
+
 	app.Listen(":8080")
 }
