e6387835b834f2b68b67ca6ec9b53cc5ca39afe0
Harrison Lloyd <harrisonalloyd@gmail.com>
2025-10-15 20:40:58 -0700

    ci test v7

--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 import "github.com/gofiber/fiber/v2"
 import "time"
+import "encoding/json"
 
 type Payload struct {
-	Message   string `json:"message"`
+	Message string `json:"message"`
 	Timestamp int64  `json:"timestamp"`
 }
 
 func root(c *fiber.Ctx) error {
-	return c.JSON(Payload{
-		Message: "My name is Harrison Lloyd",
-		Timestamp: time.Now().UnixMilli(), 
-	})
+	p := Payload{
+		Message:   "My name is Harrison Lloyd",
+		Timestamp: time.Now().UnixMilli(), // milliseconds since epoch
+	}
+	b, _ := json.Marshal(p) // Marshal is minified by default (no spaces/newlines)
+	c.Set(fiber.HeaderContentType, fiber.MIMEApplicationJSON)
+	return c.Send(b)
 }
 
 func main() {
