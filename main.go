b95e1b54f7e8390eceec4bb1f32b74fd85b1caf1
Harrison Lloyd <harrisonalloyd@gmail.com>
2025-10-15 20:31:54 -0700

    ci test, removed unsupported port input

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 import "github.com/gofiber/fiber/v2"
 import "time"
 
+type Payload struct {
+	Message   string `json:"message"`
+	Timestamp int64  `json:"timestamp"`
+}
+
 func root(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{
-		"message": "My name is Harrison Lloyd",
-		"timestamp": time.Now().UnixMilli(), 
+	return c.JSON(Payload{
+		Message: "My name is Harrison Lloyd",
+		Timestamp: time.Now().UnixMilli(), 
 	})
 }
 
